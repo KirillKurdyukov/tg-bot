52e9827c68b82c458dd484a43209345ecd9aa61f
KirillKurdyukov <kurdyukov-kir@ydb.tech>
2024-04-26 00:08:26 +0300

    Initial Tg Bot repository

---
+++ src/test/kotlin/ru/itmo/tgbot/TgBotApplicationTests.kt
@@ -0,0 +1,12 @@
+package ru.itmo.tgbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+class TgBotApplicationTests : BaseTest() {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
